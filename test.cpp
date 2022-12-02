040994c55c6e7607096976ecdd4cbad8b0bc0f46
Ezekieeel <danya.bernatavichus@inbox.ru>
2022-12-02 15:15:16 +0400

    quryBuilder

--- test.cpp
+++ test.cpp
@@ -1,7 +1,28 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include "sqlite3orm.h"
+#include <string>
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+string selectSQL(string tableName, vector <string> vfields)
+{
+
+string select = "SELECT";
+string from = "FROM";
+string fields;
+for (int i = 0; i < vfields.size(); i++)
+{  
+    fields += vfields[i] + "," + " ";
+}
+fields.pop_back();
+fields.pop_back();
+
+string result = select + " " + fields + " " + from + " "+ tableName;
+return result;
+}
 
 
 static int callback(void *data, int argc, char **argv, char **azColName)
@@ -23,14 +44,16 @@ int main(int argc, char const *argv[])
     const char *sql;
     const char *data = "Callback function called";
     
-    sql = "SELECT * from COMPANY";
-    
     user user1;
     user& user1link = user1;
 
-    sqlite3orm::getInstance()->exec(sql, callback, (void *)data);
+vector <string> vfields = {"ID", "AGE", "NAME"};
+string result = selectSQL("COMPANY", vfields);
 
-    //printf("%i\n", user1->id ); 
-    
-    return 0;
+
+const char *requestSql  = result.data();
+
+sqlite3orm::getInstance()->exec(requestSql, callback, (void *)data);
+
+return 0;
 }
