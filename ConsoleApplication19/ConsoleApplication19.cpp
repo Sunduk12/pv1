d01034d2d1168f0684bb312c1f6803fadeadb59e
Antimen <andrtveri@mail.ru>
2022-12-03 13:00:20 +0300

    Вынес класс отдельным файлом

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -6,79 +6,10 @@
 #include <vector>
 #include <exception>
 #include "../sqlite3orm.h"
-
+#include "../Sql3_scheme.h"
 
 using namespace std;
-class Sql3_scheme {
-public:
 
-    string sql3_tab(bool ifNotExists=false)
-    {
-
-        string sql = "CREATE TABLE ";
-        if (ifNotExists)
-            sql = sql + "IF NOT EXISTS ";
-            
-        sql=sql +this->table_name;
-        sql = sql + '(' + '\n';
-        for (int i = 0;i < this->fields.size();i++)
-        {
-            for (int j = 0; j < this->fields[i].size();j++)
-            {
-                sql = sql + this->fields[i][j] + "    ";
-            }
-            if (this->fields.size() - 1 == i)
-                sql = sql + '\n';
-            else
-                sql = sql + ',' + '\n';
-        }
-        sql = sql + ");";
-        return sql;
-    }
-    string drop()
-    {
-        string sql="DROP TABLE "+ this->table_name;
-        return sql;
-    }
-    string rename(string newTableName)
-    {
-        string sql = "ALTER TABLE " + this->table_name + " RENAME TO " + newTableName;
-        return sql;
-    }
-    string truncate()
-    {
-        string sql = "TRUNCATE TABLE " + this->table_name;
-        return sql;
-    }
-    Sql3_scheme* table(string table_name)
-    {
-        this->table_name = table_name;
-        return this;
-    }
-    Sql3_scheme* add_field(string field_name, string field_type) 
-    {
-        int e = 1;
-
-        
-        for (int i = 0;i < 5;i++)
-        {
-            if (field_type == type_field[i]) 
-            {
-                vector<string>field = { field_name, field_type };
-                this->fields.push_back(field);
-                e = 0;
-                break;
-            }
-        }
-        if (e) 
-            throw exception("Wrong field type! Please use types: INTEGER, BLOB, REAL, NUMERIC, TEXT");
-        return this;
-    }
-private:
-    string table_name;
-    vector<vector<string>> fields;
-    string type_field[5] = {"INTEGER","BLOB","REAL","NUMERIC","TEXT" };
-};
 
 static int callback(void* data, int argc, char** argv, char** azColName)
 {
