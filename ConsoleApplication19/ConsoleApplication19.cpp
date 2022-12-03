d80b50d8961d8eac19c02b40ed1d0b51cdfe334e
Antimen <andrtveri@mail.ru>
2022-12-03 11:50:39 +0300

    Рабочее создание таблицы

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <vector>
 #include <exception>
+#include "../sqlite3orm.h"
+
+
 using namespace std;
 class Sql3_scheme {
 public:
@@ -19,17 +22,20 @@ public:
             {
                 sql = sql + this->fields[i][j] + "    ";
             }
-            sql = sql + '\n';
+            if (this->fields.size() - 1 == i)
+                sql = sql + '\n';
+            else
+                sql = sql + ',' + '\n';
         }
         sql = sql + ");";
         return sql;
     }
-    Sql3_scheme table(string table_name)
+    Sql3_scheme* table(string table_name)
     {
         this->table_name = table_name;
-        return *this;
+        return this;
     }
-    Sql3_scheme add_field(string field_name, string field_type) 
+    Sql3_scheme* add_field(string field_name, string field_type) 
     {
         int e = 1;
 
@@ -46,7 +52,7 @@ public:
         }
         if (e) 
             throw exception("Wrong field type! Please use types: INTEGER, BLOB, REAL, NUMERIC, TEXT");
-        return *this;
+        return this;
     }
 private:
     string table_name;
@@ -54,13 +60,35 @@ private:
     string type_field[5] = {"INTEGER","BLOB","REAL","NUMERIC","TEXT" };
 };
 
+static int callback(void* data, int argc, char** argv, char** azColName)
+{
+    int i;
+    fprintf(stderr, "%s: \n", (const char*)data);
+    //data.id = i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    Sql3_scheme new_scheme;
-    cout << new_scheme.add_field("age", "INTEGER").add_field("name", "text").add_field("address","BLOB").table("chello").sql3_tab();
-
+    const char* data = "dsfdg";
+    Sql3_scheme* new_scheme = new Sql3_scheme;
+   // cout << new_scheme->add_field("age", "INTEGER")->add_field("name", "text")->add_field("address","BLOB")->table("chello")->sql3_tab();
+   string sql =  new_scheme
+       ->add_field("age", "INTEGER")
+       ->add_field("name","TEXT")
+       ->add_field("address", "BLOB")
+       ->table("chello")
+       ->sql3_tab();
   //  new_scheme.add_field("name", "TEXT");
-    
+   const char* sql1 = sql.data();
+   cout << sql1<<'\n';
+  sqlite3orm::getInstance()->exec(sql1,callback,(void *)data);
   //  vector< vector<string>> mass = { { "age","INT"},{"name","TEXT"},{"address","TEXT"} };
     //cout << ("chello", mass); 
    
