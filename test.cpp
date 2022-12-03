a8d1e4518275086b8ed2d6e5976bd74a06b89993
Ezekieeel <danya.bernatavichus@inbox.ru>
2022-12-03 12:23:09 +0400

    add class querySQL

--- test.cpp
+++ test.cpp
@@ -7,22 +7,50 @@
 
 using namespace std;
 
-string selectSQL(string tableName, vector <string> vfields)
+class querySQL
+{
+public:
+string selectSQL()
 {
 
-string select = "SELECT";
+
 string from = "FROM";
 string fields;
-for (int i = 0; i < vfields.size(); i++)
+for (int i = 0; i < this->vfields.size(); i++)
 {  
-    fields += vfields[i] + "," + " ";
+    fields += this->vfields[i] + "," + " ";
 }
 fields.pop_back();
 fields.pop_back();
 
-string result = select + " " + fields + " " + from + " "+ tableName;
+string result = this->queryType + " " + fields + " " + from + " "+ tableName;
 return result;
 }
+
+querySQL* select(string tableName)
+{
+this->tableName = tableName;
+this->queryType = "select";
+
+return this;
+}
+
+querySQL* addFields(string vfields)
+{
+this-> vfields.push_back(vfields);
+
+return this;
+}
+
+
+private:
+
+vector <string> vfields;
+string tableName;
+string queryType;
+
+
+};
 
 
 static int callback(void *data, int argc, char **argv, char **azColName)
@@ -47,13 +75,17 @@ int main(int argc, char const *argv[])
     user user1;
     user& user1link = user1;
 
-vector <string> vfields = {"ID", "AGE", "NAME"};
-string result = selectSQL("COMPANY", vfields);
+querySQL *a = new querySQL();
+// vector <string> vfields = {"ID", "AGE", "NAME"};
+// string result = a->selectSQL("COMPANY", vfields);
 
+
+
+string result = a->select("COMPANY")->addFields("NAME")->addFields("AGE")->selectSQL();
 
 const char *requestSql  = result.data();
-
 sqlite3orm::getInstance()->exec(requestSql, callback, (void *)data);
+
 
 return 0;
 }
